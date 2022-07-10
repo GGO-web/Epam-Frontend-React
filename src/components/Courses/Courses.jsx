f84e6644d19cc0648e8e53b7f866a8fa0f451c79
Andriy Oleksievets <aandriy335@gmail.com>
2022-07-10 23:33:37 +0300

    Fixed 'Add new course' button to prevent request to server when user is not an admin

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,80 +1,94 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import './Courses.css';
+import "./Courses.css";
 
-import { getAuthors } from '../../store/authors/actionCreators';
-import { gettingAuthors } from '../../services';
-import { getCoursesSelector } from '../../store/courses/selectors';
-import { getAuthorsSelector } from '../../store/authors/selectors';
-import { makeGetCoursesRequest } from '../../store/courses/thunk';
+import { getAuthors } from "../../store/authors/actionCreators";
+import { gettingAuthors } from "../../services";
+import { getCoursesSelector } from "../../store/courses/selectors";
+import { getAuthorsSelector } from "../../store/authors/selectors";
+import { getUserSelector } from "../../store/user/selectors";
+import { makeGetCoursesRequest } from "../../store/courses/thunk";
 
-import SearchBar from './components/SearchBar/SearchBar';
-import CourseCard from './components/CourseCard/CourseCard';
-import Button from '../../common/Button/Button';
-import Header from '../Header/Header';
+import SearchBar from "./components/SearchBar/SearchBar";
+import CourseCard from "./components/CourseCard/CourseCard";
+import Button from "../../common/Button/Button";
+import Header from "../Header/Header";
 
 const Courses = () => {
-	const dispatch = useDispatch();
+   const dispatch = useDispatch();
 
-	const coursesList = useSelector(getCoursesSelector);
-	const authorsList = useSelector(getAuthorsSelector);
+   const coursesList = useSelector(getCoursesSelector);
+   const authorsList = useSelector(getAuthorsSelector);
 
-	const prepareNewCourse = () => {
-		dispatch(makeGetCoursesRequest());
-	};
+   const user = useSelector(getUserSelector);
 
-	useEffect(() => {
-		// added courses & authors to the redux store when the application loads first time
-		if (coursesList.length === 0) {
-			dispatch(makeGetCoursesRequest());
-		}
+   const prepareNewCourse = (e) => {
+      if (user.role === "admin") dispatch(makeGetCoursesRequest());
+      else e.preventDefault();
+   };
 
-		if (authorsList.length === 0) {
-			gettingAuthors().then((authors) => {
-				dispatch(getAuthors(authors));
-			});
-		}
+   useEffect(() => {
+      // added courses & authors to the redux store when the application loads first time
+      if (coursesList.length === 0) {
+         dispatch(makeGetCoursesRequest());
+      }
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [dispatch]);
+      if (authorsList.length === 0) {
+         gettingAuthors().then((authors) => {
+            dispatch(getAuthors(authors));
+         });
+      }
 
-	return (
-		<>
-			<Header></Header>
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [dispatch]);
 
-			<main className='main container'>
-				<div className='course'>
-					{
-						<>
-							<div className='course-top'>
-								<SearchBar className='course-top__item'></SearchBar>
-								<div className='course-top__item'>
-									<Link to='/courses/add'>
-										<Button
-											className='course__create'
-											buttonText='Add new course'
-											data-testid='course__create'
-											onClick={() => prepareNewCourse()}
-										></Button>
-									</Link>
-								</div>
-							</div>
+   return (
+      <>
+         <Header></Header>
 
-							<ul className='course-list list-reset' data-testid='course-list'>
-								{coursesList.map((course) => {
-									return <CourseCard {...course} key={course.id}></CourseCard>;
-								})}
-							</ul>
-						</>
-					}
-				</div>
-			</main>
-		</>
-	);
+         <main className="main container">
+            <div className="course">
+               {
+                  <>
+                     <div className="course-top">
+                        <SearchBar className="course-top__item"></SearchBar>
+                        <div className="course-top__item">
+                           <Link
+                              to="/courses/add"
+                              onClick={(e) => prepareNewCourse(e)}
+                           >
+                              <Button
+                                 className="course__create"
+                                 buttonText="Add new course"
+                                 data-testid="course__create"
+                              ></Button>
+                           </Link>
+                        </div>
+                     </div>
+
+                     <ul
+                        className="course-list list-reset"
+                        data-testid="course-list"
+                     >
+                        {coursesList.map((course) => {
+                           return (
+                              <CourseCard
+                                 {...course}
+                                 key={course.id}
+                              ></CourseCard>
+                           );
+                        })}
+                     </ul>
+                  </>
+               }
+            </div>
+         </main>
+      </>
+   );
 };
 
 export default Courses;
